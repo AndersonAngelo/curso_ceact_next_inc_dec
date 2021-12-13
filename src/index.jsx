dff81a7a013177c2021bffbb02d5b7dcebebd1a5
Anderson Ângelo <anderson_angelo1@hotmail.com>
2021-12-13 01:14:14 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CounterContextProvider } from './contexts/CounterContext';
+import './index.css';
+import { Home } from './templates/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
